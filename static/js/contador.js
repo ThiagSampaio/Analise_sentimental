eec7f4c31abbfd23f3e08ce0008f6f6255f0322b
Gustavo Lopes <lopes.gustavojf@gmail.com>
2021-06-19 14:42:14 -0300

    Atualiza sistema

--- static/js/contador.js
+++ static/js/contador.js
@@ -1,33 +1,21 @@
-(function () {
-  const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
+const birthday = DateTime.local(2021, 6, 24);
+let updateClockIntervalId;
 
-  let birthday = "Jun 24, 2021 00:00:00",
-      countDown = new Date(birthday).getTime(),
-      x = setInterval(function() {
+function updateClock() {
+    const distance = birthday.diffNow();
+    const countdownElement = document.getElementById("countdown");
 
-        let now = new Date().getTime(),
-            distance = countDown - now;
+    countdownElement.innerText = distance.toFormat("d 'dias' h 'horas' m 'minutos' s 'segundos'");
 
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+    //do something later when date is reached
+    if (distance < 0) {
+        document.getElementById("headline").innerText = "It's my birthday!";
+        countdownElement.style.display = "none";
+        document.getElementById("content").style.display = "block";
 
-        //do something later when date is reached
-        if (distance < 0) {
-          let headline = document.getElementById("headline"),
-              countdown = document.getElementById("countdown"),
-              content = document.getElementById("content");
+        clearInterval(updateClockIntervalId);
+    }
+}
 
-          headline.innerText = "It's my birthday!";
-          countdown.style.display = "none";
-          content.style.display = "block";
-
-          clearInterval(x);
-        }
-        //seconds
-      }, 0)
-  }());
+updateClockIntervalId = setInterval(updateClock, 1000);
+updateClock();
